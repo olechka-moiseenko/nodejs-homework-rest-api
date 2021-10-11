fb8d71e4d96a303a6dc063d49bfd1064eead9905
Olechka Moiseenko <olechka.moiseenko@gmail.com>
2021-10-11 13:12:02 +0300

    fixed error in code

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,16 +1,16 @@
 const { NotFound } = require('http-errors')
 
 const { sendSuccessRes } = require('../helpers')
-const contactsOperations = require('../model/contacts')
+const { Contact } = require('../models')
 
 const listContacts = async(req, res) => {
-  const result = await contactsOperations.listContacts()
+  const result = await Contact.find({}, '_id name email phone favorite')
   sendSuccessRes(res, { result })
 }
 
-const getContactById = async (req, res, next) => {
+const getContactById = async (req, res) => {
   const { contactId } = req.params
-  const result = await contactsOperations.getContactById(contactId)
+  const result = await Contact.findById(contactId, '_id name email phone favorite')
   if (!result) {
     throw new NotFound(`Contact with id=${contactId} not found`)
   }
@@ -18,26 +18,36 @@ const getContactById = async (req, res, next) => {
 }
 
 const addContact = async (req, res) => {
-  const result = await contactsOperations.addContact(req.body)
-  sendSuccessRes(res, { data: result }, 201)
+  const result = await Contact.create(req.body)
+  sendSuccessRes(res, { result }, 201)
 }
 
 const updateContact = async(req, res) => {
   const { contactId } = req.params
-  const result = await contactsOperations.updateContact(contactId, req.body)
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true })
   if (!result) {
     throw new NotFound(`Contact with id=${contactId} not found`)
   }
-  sendSuccessRes(res, { result })
+  sendSuccessRes(res, result, 200, 'Contact has been successfully updated')
 }
 
-const removeContact = async (req, res, next) => {
+const updateFavoriteContact = async (req, res) => {
   const { contactId } = req.params
-  const result = await contactsOperations.removeContact(contactId)
+  const { favorite } = req.body
+  const result = await Contact.findByIdAndUpdate(contactId, { favorite }, { new: true })
+  if (result) {
+    throw NotFound(`Contact with id=${contactId} not found`)
+  }
+  sendSuccessRes(res, result, 200, 'Contact has been successfully updated')
+}
+
+const removeContact = async (req, res) => {
+  const { contactId } = req.params
+  const result = await Contact.findByIdAndDelete(contactId)
   if (!result) {
     throw new NotFound(`Contact with id=${contactId} not found`)
   }
-  sendSuccessRes(res, { message: 'Success delete' })
+  sendSuccessRes(res, result, 200, 'Contact has been successfully deleted')
 }
 
 module.exports = {
@@ -45,5 +55,6 @@ module.exports = {
   getContactById,
   addContact,
   updateContact,
+  updateFavoriteContact,
   removeContact
 }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const { joiSchema } = require('../../models/contact')
+const { joiSchema, updateFavoriteJoiSchema } = require('../../models/contact')
 const { controllerWrapper, validation } = require('../../middlewares')
 const { contacts: ctrl } = require('../../controllers')
 console.log(ctrl)
@@ -14,6 +14,8 @@ router.get('/:contactId', controllerWrapper(ctrl.getContactById))
 router.post('/', validation(joiSchema), controllerWrapper(ctrl.addContact))
 
 router.put('/:contactId', validation(joiSchema), controllerWrapper(ctrl.updateContact))
+
+router.patch('/:contactId', validation(updateFavoriteJoiSchema), controllerWrapper(ctrl.updateFavoriteContact))
 
 router.delete('/:contactId', controllerWrapper(ctrl.removeContact))
 
