a3575e47e19c5df57cab14eb320ca4eefc6868ea
Olechka Moiseenko <olechka.moiseenko@gmail.com>
2021-10-27 11:44:01 +0300

    add gravatar

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,14 +1,16 @@
 const { User } = require('../../models')
 const { Conflict } = require('http-errors')
 const { sendSuccessRes } = require('../../helpers')
+const gravatar = require('gravatar')
 
 const signup = async(req, res) => {
-  const { email, password } = req.body
+  const { email, password, subscription } = req.body
   const user = await User.findOne({ email })
   if (user) {
     throw new Conflict('Already signup')
   }
-  const newUser = new User({ email })
+  const avatarURL = gravatar.url(email)
+  const newUser = new User({ email, subscription, avatarURL })
   newUser.setPassword(password)
   await newUser.save()
   sendSuccessRes(res, null, 201)
